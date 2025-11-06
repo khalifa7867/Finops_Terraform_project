1ea6df022856eca9f989e6a44421fef09bc4b91a
Shiak Khalifa B <shiakhussiankhalifa@gmail.com>
2025-11-06 11:45:20 +0530

    Update public IP output to handle multiple instances
    Refactor output for public IP addresses to use a map of instances.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
 output "public-ip-address" {
-  value = aws_instance.example.public_ip
+ # value = aws_instance.My_EC2.public_ip
+ value = { for instance, value in aws_instance.My_EC2 :
+   instance => {
+       name = value.tags["name"]
+       public_ip_details = value.public_ip
+   }
+ }
 }
+
+#output "instance_ips" {
+  #value = [for inst in aws_instance.My_EC2 : inst.public_ip]
+#}
